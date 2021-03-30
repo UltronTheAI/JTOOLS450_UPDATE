2c91b04dd92710c415db6b63df9286bea9e7e360
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2021-03-29 22:38:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function py()
+{
+    window.open("https://www.python.org/");
+}
+function ck() {
+    window.open("https://github.com/UltronTheAI/");
+}
+function ja() {
+    window.open("https://www.java.com/en/");
+}
